0d39104f2cbb3844913c44ac99ccd0161cce6385
PlantBuilder <cavanaughjames@gmail.com>
2013-10-01 21:46:22 -0400

    blocks jumping

--- 37signals/application.js
+++ 37signals/application.js
@@ -14,10 +14,11 @@ $(document).ready(function(){
 //        $("div#overlay").html( "left: " + Math.floor(offset.left) + ", top: " + Math.floor(offset.top) );
 
           $("div.hero").html('<span>Basecamp</span> is the project management tool you <br>wish you had on your last project.');
-          $("div.hero").css({'fontSize':'28pt', 'marginTop': '18px','marginBottom':'22px'});
+          $("div.hero").css({'fontSize':'28pt', 'marginTop': '18px','marginBottom':'23px'});
           $("div.subtext").html('Are you still managing projects with email? Are you still using excel for your to-do lists? It\'s time to upgrade to<br>' +
           'Basecamp. Manage projects and collaborate with your team and clients the modern way.');
-          $("div.subtext").css({'fontFamily':'arial','fontWeight':'500','fontSize':'11pt', 'marginTop': '15px','marginBottom':'15px'});
+          $("div.subtext").css({'fontFamily':'arial','fontWeight':'500','fontSize':'11pt', 'marginTop': '17px','marginBottom':'16px'});
+          $("div.col-1-3").css({paddingTop: '2px'});
     });
     $(".module#two").mouseenter(function(){
         var offset = $(".module#two img").offset();
@@ -25,10 +26,11 @@ $(document).ready(function(){
         $("#overlay").css({'display':'block','background':'url("images/arrow-left.png")',
             'marginLeft': foo, 'marginTop': '-10px'});
             $("div.hero").html('<span>Highrise</span> remembers the important things <br>about people you\'d normally forget.');
-            $("div.hero").css({'fontSize':'28pt', 'marginTop': '18px','marginBottom':'22px'});
+            $("div.hero").css({'fontSize':'28pt', 'marginTop': '18px','marginBottom':'23px'});
             $("div.subtext").html('Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, <br>' +
             'what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise');
-            $("div.subtext").css({'fontFamily':'arial','fontWeight':'500','fontSize':'11pt', 'marginTop': '15px','marginBottom':'15px'});
+            $("div.subtext").css({'fontFamily':'arial','fontWeight':'500','fontSize':'11pt', 'marginTop': '17px','marginBottom':'16px'});
+            $("div.col-1-3").css({paddingTop: '3px'});
     });
     $(".module#three").mouseenter(function(){
         var offset = $(".module#three img").offset();
@@ -36,18 +38,20 @@ $(document).ready(function(){
         $("#overlay").css({'display':'block','background':'url("images/arrow-right.png")',
             'marginLeft': foo, 'marginTop': '-10px'});
         $("div.hero").html('From near or far, <span>Campfire</span> helps teams work <br>together over the web in real-time.');
-        $("div.hero").css({'fontSize':'28pt', 'marginTop': '18px','marginBottom':'22px'});
+        $("div.hero").css({'fontSize':'28pt', 'marginTop': '18px','marginBottom':'23px'});
         $("div.subtext").html('Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time <br>' +
             'private chat room. It\'s game changing. We couldn\'t run our own business without Campfire');
-        $("div.subtext").css({'fontFamily':'arial','fontWeight':'500','fontSize':'11pt', 'marginTop': '15px','marginBottom':'15px'});
+        $("div.subtext").css({'fontFamily':'arial','fontWeight':'500','fontSize':'11pt', 'marginTop': '17px','marginBottom':'16px'});
+        $("div.col-1-3").css({paddingTop: '3px'});
     });
     $(".module").mouseleave(function(){
         $("div.hero").html('Making collaboration productive and <br>' +
-            'enjoyable for people everyday.');
+            'enjoyable for people every day.');
         $("div.hero").css({'fontSize':'40pt', 'fontWeight': '600', marginTop: '0px','marginBottom':'0px'});
         $("div.subtext").html('Frustration-free web-based apps for collaboration, sharing information, and making decisions.');
         $("div.subtext").css({'fontFamily':'Times New Roman','fontWeight':'600','fontSize':'14pt', marginTop: '25px','marginBottom':'10px'});
         $("#overlay").css({'display':'none'});
+        $("div.col-1-3").css({paddingTop: '-2px'});
         // $(".gridpad").css({marginTop: '10px'});
     });
 });
