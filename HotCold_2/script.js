d16567090d8de467a9987b7fb4bacc924865de23
PlantBuilder <cavanaughjames@gmail.com>
2013-10-18 00:43:37 -0400

    A fancier version of HotCold

--- HotCold_2/script.js
+++ HotCold_2/script.js
@@ -37,50 +37,94 @@ $(document).ready(function() {
 
     })();
 
+    var responseArray = (function () {
 
-  var currRandomNumber = myRandomNumber.getInstance();
+        var instance,
+            myArray = [['Freezing','Cold','Cool','Warm','Hot','Boiling'],[]];
 
-    $( "#test" ).text( currRandomNumber.get() );
-
-    $( "#cheatDisplay" ).text( "Please click \"Guess\"." );
-
-    $("#newGame").click(function () {
-        currRandomNumber.set();
-        $('#cheatDisplay').empty();
-        $( "#cheatDisplay" ).text( "Please click \"Guess\"." );
-        $('li').remove();
-       // TODO Comment out when finished. test stuff.
-//       $( "p" ).empty();
-//       $( "p" ).text( currRandomNumber.get() );
-    });
-
-    $("#guess").click(function () {
-        var success, num, txt;
-        num =  +(currRandomNumber.get());
-        success = +(prompt("Enter a number between 1 & 100:", "..."));
-        $('#cheatDisplay').empty();
-        $('#hot').removeClass();
-        $('#cold').removeClass();
-        if(isNaN(success)) {
-            $( "#cheatDisplay" ).text( "NUMBER please." );
-        } else if(success > 100 || success < 1) {
-            $( "#cheatDisplay" ).text( "Between 1 and 100 please." );
-        } else if(success > num) {
-            $( "#cheatDisplay" ).text( "Too High." );
-            $('#hot').addClass("highlightHot");
-        }  else if (success < num){
-            txt =  "Too Low."
-            $( "#cheatDisplay" ).text( txt );
-            $('#cold').addClass("highlightCold");
-        } else {
-            $( "#cheatDisplay" ).text( "Just Right. Again...? (Click New Game)" );
-            alert( "YOU WON!!!" );
+        function getArray () {
+            return myArray;
         }
-        var count = $("ul li").length +1;
-        $('.ul_current').append('<li> Guess ' + count + ': ' + success + ' (' + txt + ')');
+
+        function initialize () {
+            return {
+                get: getArray
+            };
+        }
+
+        return {
+            getInstance: function () {
+
+                if ( !instance ) {
+                    instance = initialize();
+                }
+
+                return instance;
+            }
+        };
+
+    })();
+
+
+    var currRandomNumber = myRandomNumber.getInstance();
+    var currResponseArray = responseArray.getInstance();
+    var response = currResponseArray.get()
+    var $cheatDisplay =  $( "#cheatDisplay" );
+    $cheatDisplay.text( "Enter a number between 1 and 100. Then click \"Guess\"." );
+
+    $("#btn_newGame").click(function () {
+        var $cheatDisplay =  $( "#cheatDisplay" );
+        currRandomNumber.set();
+        $cheatDisplay.empty();
+        document.getElementById("guessEntry").value = '';
+        $cheatDisplay.text( "Enter a number between 1 and 100. Then click \"Guess\"." );
+        $('li').remove();
+    });
+    // get some info on the game.
+    $(".icon-circle-question-mark").click(function () {
+        var $cheatDisplay =  $( "#cheatDisplay" );
+        $cheatDisplay.hide();
+        $( "#infoDisplay").show();
+    });
+    // put the info away.
+    $(".icon-remove").click(function () {
+        var $cheatDisplay =  $( "#cheatDisplay" );
+        $( "#infoDisplay").hide();
+        $cheatDisplay.show();
+
     });
 
-    $("#cheat").click(function () {
+    $("#btn_guess").click(function () {
+        var success, num, txt, count;
+        var $hot = $('#hot');
+        var $cold = $('#cold');
+        var $cheatDisplay = $('#cheatDisplay');
+        num =  +(currRandomNumber.get());
+        success = +(document.getElementById("guessEntry").value);
+        count = $("ul li").length;
+        $('#infoDisplay').hide();
+        $cheatDisplay.empty();
+        $cheatDisplay.show();
+        if(isNaN(success)) {
+            txt =  "NUMBER please.";
+        } else if(success > 100 || success < 1) {
+            txt = "Between 1 and 100 please.";
+       // if its the first time through (count == 0) Just give the appropriate
+       // response from response[0]
+        }  else {
+            txt =  responseTxt((success-num), count)
+        }
+        if(success == num) {
+            $cheatDisplay.text( "YOU WON! Play again? (Click \"New Game\")" );
+        } else {
+            $cheatDisplay.text( txt );
+        }
+        count = count+1;
+        $('.ul_current').append('<li> #' + count + ': ' + success + ' (' + txt + ')');
+    });
+
+    $("#btn_cheat").click(function () {
+        var $cheatDisplay = $('#cheatDisplay');
         var goal = currRandomNumber.get();
         function find(start, history) {
             if (start == goal)
@@ -91,11 +135,49 @@ $(document).ready(function() {
                 return find(start + 5, "(" + history + " + 5)") ||
                     find(start * 3, "(" + history + " * 3)");
         }
-            if(find(1, "1")) {
-                $('#cheatDisplay').text(find(1, "1"));
-            } else {
-                $('#cheatDisplay').text("You are on your own.");
-            }
+        if(find(1, "1")) {
+            $cheatDisplay.text(find(1, "1"));
+        } else {
+            $cheatDisplay.text("You are on your own.");
+        }
     });
 
+    function responseTxt(diff, count){
+        var ary
+        if(diff < 0){
+            diff = diff*-1
+        }
+
+        if (diff <= 10) {
+            ary = 5;
+        } else if (diff <= 25) {
+            ary = 4;
+        } else if (diff <= 40) {
+            ary = 3;
+        } else if (diff <= 55) {
+            ary = 3;
+        } else if (diff <= 70) {
+            ary = 2;
+        } else if (diff <= 85) {
+            ary = 1;
+        } else {
+            ary = 0
+        }
+
+    if(!(response[1].length)) {
+        // if it the first guess
+        response[1][0] = diff;
+        return response[0][ary];
+    } else {
+        // let them know they are moving in the right direction.
+        response[1][count] = diff;
+        $( "#test2" ).text( response[1][count-1] );
+        if (diff < response[1][count-1]) {
+         return response[0][ary] + ' - getting warmer.'
+        } else {
+            return response[0][ary] + ' - getting cooler.';
+            }
+
+        }
+    }
 });
