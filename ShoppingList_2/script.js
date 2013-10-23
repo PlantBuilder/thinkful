a308c26a78446fb5017d14cd5a71145e6badd75e
PlantBuilder <cavanaughjames@gmail.com>
2013-10-22 21:23:40 -0400

    Fixed the empy enter syndrome.

--- ShoppingList_2/script.js
+++ ShoppingList_2/script.js
@@ -6,24 +6,26 @@ $(document).ready(function () {
     $(document).on('click', '.icon-remove', function () {
         $(this).parent().remove();
     });
-    // change the look of done items.
+    // change the look of the done items.
     $(document).on('click', "input:checkbox", function () {
         if($(this).prop('checked') == true) {
-            $(this).parent().addClass('checked');
+        $(this).parent().addClass('checked');
         } else {
             $("li").removeClass('checked');
         }
     });
-    // build the li
+    // create the li. too much going on here. separate the li creation into its own function.
     $("#listEntry").on("keypress", function (event) {
         // allow user to use 'Enter Key' to trigger guess
         var which = event.which;
         if (which == 13) {
             var inp = $("#listEntry").val();
+            if(jQuery.trim(inp).length > 0) {
             // too much going on here. Should be tucked away into a function to be called.
             $('.ul_current').append('<li>  <input type="checkbox" id="c1" name="cc" /><label for c1>'  + inp + '</label><div class="icon-remove"></div></li>');
             document.getElementById("listEntry").value = '';
             return false;
+            }
         }
     });
 });
