2176d62ff1439f5e5c5625a33c7d8784b3c288c1
PlantBuilder <cavanaughjames@gmail.com>
2013-10-26 13:09:50 -0400

    More mentor suggestions

--- ShoppingList_2/script.js
+++ ShoppingList_2/script.js
@@ -3,31 +3,35 @@
  */
 
 $(document).ready(function () {
+    var $document   = $(document);
+    var $listEntry  = $("#listEntry");
     $(function  () {
         $("ul.sortable").sortable()
-    })
+    });
+
     // delete parent icon
-    $(document).on('click', '.icon-remove', function () {
-        $minusOne.trigger('click') //$minusOne.click();
+    $document.on('click', '.icon-remove', function () {
+        $minusOne.trigger('click');
         $(this).parent(1).remove();
     });
+
     // change the look of the done items.
-    $(document).on('click', "input:checkbox", function () {
+    $document.on('click', "input:checkbox", function () {
         if($(this).prop('checked') == true) $(this).parent(1).addClass('checked');
         else
         $("li").removeClass('checked');
     });
     // create the li. too much going on here. separate the li creation into its own function.
-    $("#listEntry").on("keypress", function (event) {
+    $listEntry.on("keypress", function (event) {
         // allow user to use 'Enter Key' to trigger guess
         var which = event.which;
         if (which == 13) {
-            var inp = $("#listEntry").val();
+            var inp = $listEntry.val();
             if(jQuery.trim(inp).length > 0) {
             $plusOne.trigger('click');
             // too much going on here. Should be tucked away into a function to be called.
             $('.sortable').append('<li>  <input type="checkbox" id="c1" name="cc" /><label for c1>'  + inp + '</label><div class="icon-remove"></div></li>');
-            document.getElementById("listEntry").value = '';
+            $listEntry.val('');
             return false;
             }
         }
@@ -49,6 +53,6 @@ $(document).ready(function () {
     var $reset = $('#reset').click(function() {
         favicon.reset();
     });
-    //intial value
+    //initial value
     favicon.badge(badge);
 });
