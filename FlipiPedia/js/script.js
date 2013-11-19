7881ac419e515f9bd4339280c0f068bab4420662
PlantBuilder <cavanaughjames@gmail.com>
2013-11-18 19:02:17 -0500

    First Flipipedia Commit

---
+++ FlipiPedia/js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+//    $('#demo1').WikipediaWidget('Stuttgart');
+    $('#demo1').WikipediaWidget('Albert Einstein', { 'showTitle' : false, 'maxThumbnails' : 6 });
+});
