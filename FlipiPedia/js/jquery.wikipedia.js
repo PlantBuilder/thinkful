7d914370ed9fc82db8378805779958fe9bad802d
PlantBuilder <cavanaughjames@gmail.com>
2013-11-22 08:31:17 -0500

    Adding Collage

--- FlipiPedia/js/jquery.wikipedia.js
+++ FlipiPedia/js/jquery.wikipedia.js
@@ -5,10 +5,10 @@
  */ 
 $.fn.WikipediaWidget = function(wikipediaPage, options) {  
   //init defaults
-  var showTitle = true;
-  var maxThumbnails = 3;
+  var showTitle = false;
+  var maxThumbnails = 24;
   var cutFirstInfoTableRows = 5;
-  var maxInfoTableRows = 8;
+  var maxInfoTableRows = 10;
   var thumbMaxWidth  = '180px'; 
   var thumbMaxHeight = '180px';
   
@@ -47,12 +47,15 @@ $.fn.WikipediaWidget = function(wikipediaPage, options) {
   //check if pagetitle is set
   if(wikipediaPage == undefined) { console.log('No Wikipedia search titles ! started No query!');return; }
   //init given wikiContainer from the Selector init
+    console.log(wikipediaPage)
   var wikiContainer = this;      
   wikiContainer.append('<div class="ajaxLoading"><img src="img/ajax-loader.gif"></div>');
+
   //get data.parse.images
-  $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?', {page:wikipediaPage, prop:'text|images', uselang:'en', list: 'allimages'}, function(parsedata) {
+  $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?', {page:wikipediaPage, prop:'text|images', uselang:'en'}, function(parsedata) {
     //remove loading image
     wikiContainer.find('.ajaxLoading').remove();
+    wikiContainer.find('.Collage').remove();
     //debug
     console.dir(parsedata);
     //drop text to div container
@@ -63,30 +66,48 @@ $.fn.WikipediaWidget = function(wikipediaPage, options) {
       wikiContainer.append('<div class="wikipediaTitle"></div>').find('.wikipediaTitle').html(parsedata.parse.title);
     }
     //get images of right side table
-    var rightTableImages = content.find('table a.image img');        
-    //append first image to main container  
-    wikiContainer.append($(rightTableImages).first().removeAttr('srcset').removeAttr('height').removeAttr('width').wrap('<div class="wikipediaLogo"></div>').parent());
+ //   var rightTableImages = content.find('table a.image img');
+ //     console.dir(rightTableImages);
+      //var Images = content.find('img');
+      //var thumbcontent  = content.find('.thumbinner')
+      //console.dir(thumbcontent);
+   //   $.each(rightThumbnails, function(index, Thumbnail) {
+          //add thumb with thumbMaxHeight and thumbMaxWidth
+   //       if(index<=Images.length) wikiContainer.find('.wikipediaThumbGallery').append($(Images).removeAttr('srcset').removeAttr('height').removeAttr('width').css({'width': 'auto', 'height': 'auto', 'max-width': thumbMaxWidth, 'max-height': thumbMaxHeight}).wrap('<li class="wikipediaThumbnail"></li>').parent());
+   //   });
+      console.log('to here');
+    //append first image to main container
+ //    wikiContainer.append($(rightTableImages).first().removeAttr('srcset').removeAttr('height').removeAttr('width').wrap('<div class="wikipediaLogo"></div>').parent());
     //append description to main container
-    var description = content.find('p').first().text();
-    wikiContainer.append('<div class="wikipediaDescription"></div>').find('.wikipediaDescription').append(description);
+    //var description = content.find('p').first.text() $( ":nth-child(1)" ).next( "p" ).text()
+ //     var descriptionArray = content.find('p');
+ //     console.dir(descriptionArray);
+ //     var description = descriptionArray[0].innerText + '<br>';
+ //     description +=  descriptionArray[1].innerText;
+   // wikiContainer.append('<div class="wikipediaDescription"></div>').find('.wikipediaDescription').append(description);
     //get thumbnail images
-    var rightThumbnails = content.find('.thumb a.image img');
-    wikiContainer.append('<ul class="wikipediaThumbGallery"></ul>');
+//    var rightThumbnails = content.find('.thumb a.image img');
+    var rightThumbnails = content.find('img.thumbimage');
+   // wikiContainer.append('<ul class="wikipediaThumbGallery"></ul>');
+    wikiContainer.append('<div class="Collage effect-parent"></div>');
     //add maxThumbnails to main container
     $.each(rightThumbnails, function(index, Thumbnail) {
       //add thumb with thumbMaxHeight and thumbMaxWidth
-      if(index<maxThumbnails) wikiContainer.find('.wikipediaThumbGallery').append($(Thumbnail).removeAttr('srcset').removeAttr('height').removeAttr('width').css({'width': 'auto', 'height': 'auto', 'max-width': thumbMaxWidth, 'max-height': thumbMaxHeight}).wrap('<li class="wikipediaThumbnail"></li>').parent());  
+      if(index<maxThumbnails) wikiContainer.find('.Collage').append($(Thumbnail).removeAttr('srcset').removeClass('thumbimage').wrap("<div class='Image_Wrapper' data-caption = '" + this.alt + "'></div>").parent());    //  <a href = 'http://en.wikipedia.org/wiki/" + wikipediaPage + "' target='_blank'></a>
+        console.log($(Thumbnail).attr('alt'))
     });
+    collage();
+   //   $('.Collage').collageCaption();
     //get right side table
-    var rightTable = content.find('table.infobox, table.float-right').first();
+//    var rightTable = content.find('table.infobox, table.float-right').first();
     //init new table
-    var newTable = $('<table class="wikipediaInfoTable"></table>');
+//   var newTable = $('<table class="wikipediaInfoTable"></table>');
     //parse new table from right side table with cutFirstInfoTable and maxInfoTableRows
-    $.each(rightTable.find('tr'), function(index, element) {
-      if(index>cutFirstInfoTableRows && index<(cutFirstInfoTableRows+maxInfoTableRows)) newTable.append(element);
-    });
+//    $.each(rightTable.find('tr'), function(index, element) {
+//      if(index>cutFirstInfoTableRows && index<(cutFirstInfoTableRows+maxInfoTableRows)) newTable.append(element);
+//    });
     //append new table to main container
-    wikiContainer.append(newTable);
-    wikiContainer.append($('<div class="clear"></div>'));                        
+//    wikiContainer.append(newTable);
+//    wikiContainer.append($('<div class="clear"></div>'));
   });  
 };
--- FlipiPedia/js/script.js
+++ FlipiPedia/js/script.js
@@ -1,4 +1,80 @@
-$(document).ready(function() {
-//    $('#demo1').WikipediaWidget('Stuttgart');
-    $('#demo1').WikipediaWidget('Albert Einstein', { 'showTitle' : false, 'maxThumbnails' : 6 });
+// All images need to be loaded for this plugin to work so
+// we end up waiting for the whole window to load in this example
+$(window).load(function () {
+    $(document).ready(function(){
+
+        collage();
+        $('.Collage').collageCaption();
+
+        var $btn_search =  $( "#btn_search" );
+        var $input        = $( "input[name='listInput']" );
+        $btn_search.click(function () {
+            var success, num, txt, count;
+            // grab the random number
+            if($input.val())  $('#demo2').WikipediaWidget($input.val());
+        });
+    });
 });
+
+
+// Here we apply the actual CollagePlus plugin
+function collage() {
+    $('.Collage').removeWhitespace().collagePlus(
+        {
+            'fadeSpeed'     : 2000,
+            'targetHeight'  : 200,
+            'effect'        : "effect-5"
+        }
+    );
+}
+
+// This is just for the case that the browser window is resized
+var resizeTimer = null;
+$(window).bind('resize', function() {
+    // hide all the images until we resize them
+    $('.Collage .Image_Wrapper').css("opacity", 0);
+    // set a timer to re-apply the plugin
+    if (resizeTimer) clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(collage, 200);
+});
+ /*
+    var $btn_search =  $( "#btn_search" );
+    var $input        = $( "input[name='listInput']" );
+
+    $btn_search.click(function () {
+        var success, num, txt, count;
+        // grab the random number
+       if($input.val())  $('#demo1').WikipediaWidget($input.val());
+          //console.log($inp.val());
+      //  console.log(success);
+        /*
+        // is this the first guess?
+        count = $("li").length;
+        $info.hide();
+        $sub.empty();
+        $sub.show();
+        if (success > 100 || success < 1) {
+            txt =  "Between 1 and 100.";
+        } else {
+            txt = responseTxt((success - num), count);
+        }
+        count = count+1;
+        if(success == num) {
+            $sub.text( "YOU WON! Play again?" );
+            $ul_current.prepend( '<li class="winner"> #' + count + ': ' + success + ' YOU WON!' );
+            // que player to choose New Game...
+            $btn_guess.animate({color: '#BBBBBB'}, 1000);
+            $btn_newGame.animate({color: '#F5F5F5'}, 1000);
+            // changed the look to make it more visible.
+
+        } else {
+            $sub.text( txt );
+            // Descending Order: I changed to 'prepend' as it is easier to read 'li' when current one is on top
+            $ul_current.prepend( '<li> #' + count + ': ' + success + ' (' + txt + ')' );
+        }
+
+    });
+//    $('#demo1').WikipediaWidget('Stuttgart');
+//      $('#demo1').WikipediaWidget('Albert Einstein', { 'showTitle' : true, 'maxThumbnails' : 6 });
+});
+*/
